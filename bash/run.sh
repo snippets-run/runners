23f7d1d3b4e7d92ed665ba46506f4f75b94800f7
Darlan Alves <me@darlanalv.es>
2024-02-10 00:07:45 +0100

    fix: bash runner is broken

--- bash/run.sh
+++ bash/run.sh
@@ -2,5 +2,5 @@
 
 [[ -z "$1" ]] && echo "Missing snippet name. Try 'run.sh <snippet-name>'" && exit 1
 
-curl -sS --output snippet.sh https://registry.snippets.run/shell/$1
-bash snippet.sh
+curl -sS --output $TMPDIR/snippet.sh https://registry.snippets.run/s/shell/$1
+bash $TMPDIR/snippet.sh
