88954c38ad005eceaf24ac4aae2b3b17fe0a0664
Darlan Alves <me@darlanalv.es>
2024-02-20 12:02:20 +0100

    chore: update run.sh

--- bash/run.sh
+++ bash/run.sh
@@ -2,5 +2,10 @@
 
 [[ -z "$1" ]] && echo "Missing snippet name. Try 'run.sh <snippet-name>'" && exit 1
 
+is_supported()
+{
+  command -v $1 >/dev/null 2>&1
+}
+
 curl -sS --output $TMPDIR/snippet.sh https://registry.snippets.run/s/shell/$1
 bash $TMPDIR/snippet.sh
